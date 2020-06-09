e6867144f362f1e8957c56d38da5635d8419e726
yclim95 <lyc950421@gmail.com>
2020-06-09 17:03:32 +0800

    Updaate .java

--- src/com/battleshipsgame/BattleShipsGame.java
+++ src/com/battleshipsgame/BattleShipsGame.java
@@ -27,26 +27,25 @@ public class BattleShipsGame {
 
 
         // Step 4 - Battle
-        do{
+        do {
             battle();
-        }while((PlayerShip.NUM_SHIP!=0 && ComputerShip.NUM_SHIP!=0)); // while coordinate == 0 STOP
-
+        } while ((PlayerShip.NUM_SHIP != 0 && ComputerShip.NUM_SHIP != 0)); // while coordinate == 0 STOP
 
 
         // Step 5 - Game Over
         gameOver();
     }
 
-    public static void deployShip(){
+    public static void deployShip() {
         deployPlayerShip();
         deployComputerShip();
     }
 
-    public static void deployPlayerShip(){
+    public static void deployPlayerShip() {
         int xCoordinate;
         int yCoordinate;
         System.out.println("Deploy your ships");
-        for (int i = 0; i < PlayerShip.NUM_SHIP;) {
+        for (int i = 0; i < PlayerShip.NUM_SHIP; ) {
 
             // Ask for user's input on coordinate to set the location of player's ship on the map
             System.out.print("Enter X coordinate for your " + (i + 1) + ". ship:");
@@ -59,10 +58,9 @@ public class BattleShipsGame {
             // Check if player's ship exist on the coordinates entered (based on the map)
             if (!(xCoordinate >= 0 && xCoordinate < 10) || !(yCoordinate >= 0 && yCoordinate < 10)) {
                 System.out.println("You can't place ships outside the 10 by 10 grid");
-            }else if (mapCoordinates[xCoordinate][yCoordinate] == 1){
+            } else if (mapCoordinates[xCoordinate][yCoordinate] == 1) {
                 System.out.println("You can NOT place 2 or more ships on the same location");
-            }
-            else {
+            } else {
                 mapCoordinates[xCoordinate][yCoordinate] = 1;
                 i++; // Player ship exist on the coordinate entered (based on the map)
             }
@@ -72,11 +70,11 @@ public class BattleShipsGame {
         map.printMap(mapCoordinates); //print the ship location on the map
     }
 
-    public static void deployComputerShip(){
+    public static void deployComputerShip() {
         int xCoordinate;
         int yCoordinate;
         System.out.println("Computer is Deploying ships");
-        for (int i = 0; i < ComputerShip.NUM_SHIP;) {
+        for (int i = 0; i < ComputerShip.NUM_SHIP; ) {
 
 
             // set random coordinate to set the location of computer's ships (5) on the map
@@ -87,10 +85,10 @@ public class BattleShipsGame {
             yCoordinate = index;
 
             // Check if player's ship or computer's sh exist on the coordinates entered (based on the map)
-            if (mapCoordinates[xCoordinate][yCoordinate] != 2 || mapCoordinates[xCoordinate][yCoordinate] != 1) {
+            if (mapCoordinates[xCoordinate][yCoordinate] == 0) {
                 mapCoordinates[xCoordinate][yCoordinate] = 2;
                 i++; // Player ship exist on the coordinate entered (based on the map)
-                System.out.println((i+1) + ". Ship DEPLOYED");
+                System.out.println((i) + ". Ship DEPLOYED");
             }
 
         }
@@ -99,7 +97,7 @@ public class BattleShipsGame {
         map.printMap(mapCoordinates); //print the ship location on the map
     }
 
-    public static void playerTurn(){
+    public static void playerTurn() {
         // Step 4.1 - Player's Turn
         /*
          * 4.1.2 Player correctly guessed coordinates of computer’s ship (computer loses ship).
@@ -114,40 +112,42 @@ public class BattleShipsGame {
          */
         int xCoordinate;
         int yCoordinate;
-        do{
-            System.out.println("YOUR TURN");
+        do {
+            System.out.println("\nYOUR TURN");
             System.out.print("Enter X coordinate: ");
             xCoordinate = input.nextInt();
             System.out.print("Enter Y coordinate: ");
             yCoordinate = input.nextInt();
 
             //Check if is out of range or have been chosen
-            if(!(xCoordinate >= 0 && xCoordinate < 10) || !(yCoordinate >= 0 && yCoordinate < 10))
-                System.out.println("Invalid input. Re-enter valid coordinate [0-9]!");
-            else{
-                if (mapCoordinates[xCoordinate][yCoordinate] == 2){
-                    System.out.println("Boom! You sunk the ship!");
+            if (!(xCoordinate >= 0 && xCoordinate < 10) || !(yCoordinate >= 0 && yCoordinate < 10))
+                System.out.println("\nInvalid input. Re-enter valid coordinate [0-9]!");
+            else {
+                if (mapCoordinates[xCoordinate][yCoordinate] == 2) {
+                    System.out.println("\nBoom! You sunk the ship!");
                     mapCoordinates[xCoordinate][yCoordinate] = 3; // means "!" / mark as 'sunk ship'
                     ComputerShip.NUM_SHIP--;
                     computerShip.setScore(ComputerShip.NUM_SHIP);
-                }else if (mapCoordinates[xCoordinate][yCoordinate] == 1){
-                    System.out.println("Oh no, you sunk your own ship :(");
+                } else if (mapCoordinates[xCoordinate][yCoordinate] == 1) {
+                    System.out.println("\nOh no, you sunk your own ship :(");
                     mapCoordinates[xCoordinate][yCoordinate] = 4; // means "x" / mark as 'player loses ship'
                     PlayerShip.NUM_SHIP--;
                     playerShip.setScore(PlayerShip.NUM_SHIP);
                     computerShip.setScore(ComputerShip.NUM_SHIP + 1);
-                }
-                else{
-                    System.out.println("Sorry, you missed");
+                } else if (mapCoordinates[xCoordinate][yCoordinate] == 3 || mapCoordinates[xCoordinate][yCoordinate] == 4 ||
+                        mapCoordinates[xCoordinate][yCoordinate] == 5)
+                    System.out.println("\nHave entered repeated coordinates. Please re-enter: ");
+                else {
+                    System.out.println("\nSorry, you missed");
                     mapCoordinates[xCoordinate][yCoordinate] = 5; // means "-" / mark as 'missed'
                 }
             }
-        }while(!(xCoordinate >= 0 && xCoordinate < 10) || !(yCoordinate >= 0 && yCoordinate < 10));
+        } while (!(xCoordinate >= 0 && xCoordinate < 10) || !(yCoordinate >= 0 && yCoordinate < 10));
 
         map.setCoordinates(mapCoordinates); //update Coordinate mark on the map
     }
 
-    public static void computerTurn(){
+    public static void computerTurn() {
         // Step 4.2 - Computer's Turn
         /*
          * 4.2.1 Computer guessed coordinates of the player’s ship (player loses ship).
@@ -164,8 +164,8 @@ public class BattleShipsGame {
          */
         int xCoordinate;
         int yCoordinate;
-        do{
-            System.out.println("COMPUTER'S TURN");
+        do {
+            System.out.println("\nCOMPUTER'S TURN");
             // set random coordinate
             Random rand = new Random();
             int index = rand.nextInt(10); // random index between [0 - 10]
@@ -175,32 +175,35 @@ public class BattleShipsGame {
 
 
             //Check if is out of range or have been chosen
-            if(!(xCoordinate >= 0 && xCoordinate < 10) || !(yCoordinate >= 0 && yCoordinate < 10))
-                System.out.println("Invalid input. Re-enter valid coordinate [0-9]!");
-            else{
-                if (mapCoordinates[xCoordinate][yCoordinate] == 1){
-                    System.out.println("The Computer sunk one of your ships!");
+            if (!(xCoordinate >= 0 && xCoordinate < 10) || !(yCoordinate >= 0 && yCoordinate < 10))
+                System.out.println("\nInvalid input. Re-enter valid coordinate [0-9]!");
+            else {
+                if (mapCoordinates[xCoordinate][yCoordinate] == 1) {
+                    System.out.println("\nThe Computer sunk one of your ships!");
                     mapCoordinates[xCoordinate][yCoordinate] = 4; // mark "x" / mark as 'player loses ship'
                     PlayerShip.NUM_SHIP--;
                     playerShip.setScore(PlayerShip.NUM_SHIP);
-                }else if (mapCoordinates[xCoordinate][yCoordinate] == 2){
-                    System.out.println("The Computer sunk one of its own ships");
+                } else if (mapCoordinates[xCoordinate][yCoordinate] == 2) {
+                    System.out.println("\nThe Computer sunk one of its own ships");
                     mapCoordinates[xCoordinate][yCoordinate] = 3; // means "!" / mark as 'computer loses ship'
                     ComputerShip.NUM_SHIP--;
                     computerShip.setScore(ComputerShip.NUM_SHIP);
                     playerShip.setScore(PlayerShip.NUM_SHIP + 1);
-                }else {
-                    System.out.println("Computer missed");
+                } else if (mapCoordinates[xCoordinate][yCoordinate] == 3 || mapCoordinates[xCoordinate][yCoordinate] == 4 ||
+                        mapCoordinates[xCoordinate][yCoordinate] == 5) {
+                    System.out.print("");
+                } else {
+                    System.out.println("\nComputer missed");
                     mapCoordinates[xCoordinate][yCoordinate] = 6; // means "-" / mark as 'missed'
                 }
             }
 
-        }while(!(xCoordinate >= 0 && xCoordinate < 10) || !(yCoordinate >= 0 && yCoordinate < 10));
+        } while (!(xCoordinate >= 0 && xCoordinate < 10) || !(yCoordinate >= 0 && yCoordinate < 10));
 
         map.setCoordinates(mapCoordinates); //update coordinate mark on the map
     }
 
-    public static void battle(){
+    public static void battle() {
         playerTurn();
         computerTurn();
         map.printMap(mapCoordinates); //print the ship location on the map
@@ -210,11 +213,11 @@ public class BattleShipsGame {
         System.out.println();
     }
 
-    public static void gameOver(){
+    public static void gameOver() {
         System.out.println("Your ships: " + PlayerShip.NUM_SHIP + " | Computer ships: " + ComputerShip.NUM_SHIP);
         System.out.println("Player Score: " + playerShip.getScore() + " | Computer Score: " + computerShip.getScore());
 
-        if (ComputerShip.NUM_SHIP==0)
+        if (ComputerShip.NUM_SHIP == 0)
             System.out.println("Hooray! You win the battle\n");
         else
             System.out.println("You lose the battle\n");
